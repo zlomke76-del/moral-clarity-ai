b23c3ecb7282c711f1d6a57f4a4d9c6a1dc91d91
zlomke76-del <zlomke76@gmail.com>
2025-09-29 17:51:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,18 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Privacy from "./Privacy";
+import Terms from "./Terms";
 
-import App from './App.jsx'
-import Privacy from './Privacy.jsx'
-import Terms from './Terms.jsx'
-import ScrollToTop from './components/ScrollToTop.jsx'
-
-function Root() {
-  return (
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
-  )
-}
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Root />
   </React.StrictMode>
-)
+);
