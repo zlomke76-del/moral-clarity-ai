391d1a4dab55eb9ea02589216225c3a129d3bdea
zlomke76-del <zlomke76@gmail.com>
2025-09-30 17:43:42 -0500

    Delete app directory

--- app/canceled/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Canceled() {
-  return (
-    <main style={{ maxWidth: 720, margin: "40px auto", fontFamily: "system-ui, sans-serif" }}>
-      <h1>Checkout canceled</h1>
-      <p>No charge was made.</p>
-    </main>
-  );
-}
