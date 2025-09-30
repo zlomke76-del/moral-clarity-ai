533d764a483549fcd5deed3a37f5b1b3a5fba8b6
zlomke76-del <zlomke76@gmail.com>
2025-09-30 17:44:13 -0500

    Delete src directory

--- src/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import NotFound from "./NotFound.jsx";
-
-<Routes>
-  <Route path="/" element={<App />} />
-  <Route path="/privacy" element={<Privacy />} />
-  <Route path="/terms" element={<Terms />} />
-  <Route path="*" element={<NotFound />} />   {/* fallback */}
-</Routes>
