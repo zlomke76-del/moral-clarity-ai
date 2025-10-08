8d9a92b44d97bf830eeee698ae46d799bde67acd
zlomke76-del <zlomke76@gmail.com>
2025-10-08 07:06:24 -0500

    Create supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,14 @@
+// lib/supabaseClient.ts
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: true,
+    },
+  }
+);
