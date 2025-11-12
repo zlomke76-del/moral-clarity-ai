50aa83f01ff51114a6219964f8451430688263fc
zlomke76-del <zlomke76@gmail.com>
2025-11-12 11:20:19 -0600

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,8 @@
 // app/app/page.tsx
 import FeatureGrid from "@/app/components/FeatureGrid";
 
+export const runtime = "nodejs";
+
 export default function AppHome() {
   return (
     <div className="min-h-[60vh]">
@@ -9,4 +11,3 @@ export default function AppHome() {
     </div>
   );
 }
-
