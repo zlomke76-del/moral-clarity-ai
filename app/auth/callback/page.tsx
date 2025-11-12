429a1e0bf861908dc2634850701a7eb3f89741fc
zlomke76-del <zlomke76@gmail.com>
2025-11-12 19:36:17 +0000

    Remove duplicate /auth/callback page; use server route handler

--- app/auth/callback/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/auth/callback/page.tsx
-export const dynamic = 'force-dynamic';
-
-import CallbackShell from '@/components/CallbackShell';
-export default function CallbackPage() {
-  return <CallbackShell />;
-}
