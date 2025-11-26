bfcd3bb719194433f943831cfe47614f5d916c17
zlomke76-del <zlomke76@gmail.com>
2025-11-25 23:23:22 -0600

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,8 +1,8 @@
 // app/app/page.tsx
 import NeuralShell from "@/app/components/NeuralShell";
+import FeatureGrid from "@/app/components/FeatureGrid";
 import NeuralSidebar from "@/app/components/NeuralSidebar";
 import BrainHero from "@/app/components/BrainHero";
-import FeatureGrid from "@/app/components/FeatureGrid";
 
 export const runtime = "nodejs";
 
@@ -10,7 +10,7 @@ export default function AppHome() {
   return (
     <NeuralShell>
       {/* Hero: neural sidebar + brain center */}
-      <section className="flex w-full flex-col gap-6 md:grid md:grid-cols-[260px,minmax(0,1fr)] md:items-stretch">
+      <section className="flex w-full max-w-6xl flex-col gap-6 md:mx-auto md:grid md:grid-cols-[260px,minmax(0,1fr)] md:items-stretch">
         {/* Left neural nav */}
         <NeuralSidebar />
 
@@ -18,8 +18,8 @@ export default function AppHome() {
         <BrainHero />
       </section>
 
-      {/* Workspaces grid below hero */}
-      <section className="mt-10 md:mt-12">
+      {/* Neural workspaces grid below hero */}
+      <section className="mx-auto mt-12 w-full max-w-6xl pb-16">
         <FeatureGrid />
       </section>
     </NeuralShell>
