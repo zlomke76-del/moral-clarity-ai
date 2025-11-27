366d227a9d7516911e69b3227b3195080a278a7e
zlomke76-del <zlomke76@gmail.com>
2025-11-27 08:56:25 -0600

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,12 +3,19 @@
 
 import SolaceDock from "@/app/components/SolaceDock";
 import NeuralShell from "@/app/components/NeuralShell";
+import NeuralSidebar from "@/app/components/NeuralSidebar";
 
 export default function AppHome() {
   return (
     <NeuralShell>
-      <div className="flex min-h-screen items-center justify-center">
-        <SolaceDock />
+      <div className="flex min-h-screen items-stretch">
+        {/* Left: Neural sidebar (chip-style cards) */}
+        <NeuralSidebar />
+
+        {/* Right: main Solace workstation area */}
+        <main className="flex flex-1 items-center justify-center px-4 py-6">
+          <SolaceDock />
+        </main>
       </div>
     </NeuralShell>
   );
