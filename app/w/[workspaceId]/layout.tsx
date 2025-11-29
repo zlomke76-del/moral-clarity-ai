d135dfc95f1026a9cb751108f32a93a7ad3a7c63
zlomke76-del <zlomke76@gmail.com>
2025-11-28 21:50:30 -0600

    Refactor WorkspaceLayout component structure

--- app/w/[workspaceId]/layout.tsx
+++ app/w/[workspaceId]/layout.tsx
@@ -3,25 +3,21 @@
 
 import type { ReactNode } from "react";
 import NeuralSidebar from "@/app/components/NeuralSidebar";
-import { SupabaseSessionProvider } from "@/app/providers/supabase-session";
 
 export default function WorkspaceLayout({
   children,
 }: {
   children: ReactNode;
-  params: { workspaceId: string };
 }) {
   return (
-    <SupabaseSessionProvider>
-      {/* 👇 This is your flex parent: sidebar + main content */}
-      <div className="min-h-screen flex bg-[#020617]">
-        <NeuralSidebar />
+    <div className="min-h-screen w-full flex flex-row">
+      {/* Left Sidebar */}
+      <NeuralSidebar />
 
-        <main className="flex-1 flex flex-col items-stretch justify-start">
-          {children}
-        </main>
-      </div>
-    </SupabaseSessionProvider>
+      {/* Main Content */}
+      <main className="flex-1 min-h-screen p-8">
+        {children}
+      </main>
+    </div>
   );
 }
-
