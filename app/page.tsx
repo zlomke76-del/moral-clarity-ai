17c5b1a6b7257297e099c6c290a7df66b8731f5f
zlomke76-del <zlomke76@gmail.com>
2025-11-28 18:53:34 -0600

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-import { redirect } from "next/navigation";
-
-export default function HomePage() {
-  // Always go to the main workspace
-  redirect("/w/" + process.env.NEXT_PUBLIC_MCA_WORKSPACE_ID);
-}
-
-
