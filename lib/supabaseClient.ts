5d6ae88b4bfcff3c5c8d4698f2ff3cab5838558e
zlomke76-del <zlomke76@gmail.com>
2025-11-29 09:59:39 -0600

    Delete lib/supabaseClient.ts

--- lib/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-// /lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
-
-export default supabase;
