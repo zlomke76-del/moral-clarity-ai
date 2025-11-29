70b8d16e4288226078e8984c102c475f12f6b6a5
zlomke76-del <zlomke76@gmail.com>
2025-11-28 18:39:34 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className="mc-root h-full">
-        {/* ❗ REMOVE ALL EMPTY WRAP DIVS */}
+        {/* IMPORTANT: No wrapper divs above children */}
         <SupabaseProvider>
           {children}
         </SupabaseProvider>
