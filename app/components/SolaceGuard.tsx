c61e3aa9b42873a710b8edb5ddecd3fd1a8628b5
zlomke76-del <zlomke76@gmail.com>
2025-12-04 07:21:13 -0600

    Update SolaceGuard.tsx

--- app/components/SolaceGuard.tsx
+++ app/components/SolaceGuard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import SolaceDockLoader from "@/components/SolaceDockLoader";
+import SolaceDockLoader from "@/app/components/SolaceDockLoader";
 
 export default function SolaceGuard() {
   const pathname = usePathname() || "";
