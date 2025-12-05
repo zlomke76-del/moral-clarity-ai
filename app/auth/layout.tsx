da5ede60b7b6c8b8b4fc57f17d502ef920807329
zlomke76-del <zlomke76@gmail.com>
2025-12-05 12:33:58 -0600

    Update layout.tsx

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,7 +1,14 @@
+// app/auth/layout.tsx
+"use client";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center px-6 py-24">
+      {children}
+    </div>
+  );
 }
