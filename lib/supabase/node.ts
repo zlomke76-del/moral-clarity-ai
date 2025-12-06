f1ee469980274be26b66ca4aa4d7a5daad42831a
zlomke76-del <zlomke76@gmail.com>
2025-12-06 15:47:41 -0600

    Update node.ts

--- lib/supabase/node.ts
+++ lib/supabase/node.ts
@@ -1,11 +1,9 @@
 // lib/supabase/node.ts
-// NODE-ONLY Supabase client (service key)
-
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseNode = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!,
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
   {
     auth: { persistSession: false },
   }
