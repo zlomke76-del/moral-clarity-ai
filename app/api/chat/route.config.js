9707dc0db1ac4f476791f10dfa650d78da01b47f
zlomke76-del <zlomke76@gmail.com>
2025-12-06 15:10:34 -0600

    Create route.config.js

---
+++ app/api/chat/route.config.js
@@ -0,0 +1,6 @@
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+export const preferredRegion = "auto";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+export const skipMiddleware = true;
