07ac4bef6e6d867dc3d37b763c3053c2015be238
zlomke76-del <zlomke76@gmail.com>
2025-12-28 16:52:12 -0600

    Update layout.tsx

--- app/w/layout.tsx
+++ app/w/layout.tsx
@@ -1,6 +1,20 @@
-export default function WorkspaceLayout({ children }) {
+// app/w/layout.tsx
+
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+type WorkspaceLayoutProps = {
+  children: ReactNode;
+};
+
+export default function WorkspaceLayout({
+  children,
+}: WorkspaceLayoutProps) {
   return (
-    <div className="w-full h-full px-0 py-0">
+    <div
+      data-app-content
+      className={clsx("w-full h-full px-0 py-0")}
+    >
       {children}
     </div>
   );
