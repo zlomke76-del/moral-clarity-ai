9bda3f14387e2a5fac0a22f8a8c8225a424f7410
zlomke76-del <zlomke76@gmail.com>
2025-12-28 17:07:28 -0600

    Update layout.tsx

--- app/w/[workspaceId]/memory/layout.tsx
+++ app/w/[workspaceId]/memory/layout.tsx
@@ -1,9 +1,13 @@
+// app/w/[workspaceId]/memory/layout.tsx
 import { ReactNode } from "react";
+
+export const dynamic = "force-dynamic";
 
 export default function WorkspaceMemoryLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  // DO NOT validate params here
   return <>{children}</>;
 }
